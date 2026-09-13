95e0bc67f657e50578f308a9577339baf9562ec6
farmoretype <farmoretype1@gmail.com>
2026-09-13 11:11:10 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,11 +4,12 @@ local main_event = replicated_storage:WaitForChild("MainEvent")
 task.spawn(function()
     local codes = {
         "DOG",
-        "SHARK"
+        "SHARK",
+        "BUFF"
     }
 
     for _, code in ipairs(codes) do
         main_event:FireServer("EnterPromoCode", code)
-        task.wait(6)
+        wait(6)
     end
 end)
